0d6ac4f947b1e799d82e1cbfe485ca7edeb60672
Vinicius M <matias.uplink@outlook.com>
2021-10-28 10:26:51 -0300

    criando repositorio e atualizando application.properties

--- src/main/java/com/edoe/api/Application.java
+++ src/main/java/com/edoe/api/Application.java
@@ -1,13 +1,26 @@
 package com.edoe.api;
 
+import com.edoe.api.entity.Role;
+import com.edoe.api.entity.User;
+import com.edoe.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+@Autowired
+	UserRepository userRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User(null, "teste@teste123", "8399353038", "Igreja", "teste", null);
+		User user2 = new User(null, "teste@teste123", "8399353038", "Igreja", "teste", Role.ADMIN);
+		userRepository.save(user1);
+		userRepository.save(user2);
+	}
 }
